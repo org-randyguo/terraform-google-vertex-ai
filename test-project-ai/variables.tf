d90e9ae6984e69922a2101f864f038a778f7c45a
Randy Guo <randyzwguo@qq.com>
2024-09-18 10:22:34 +0800

    Update variables.tf

--- test-project-ai/variables.tf
+++ test-project-ai/variables.tf
@@ -7,7 +7,7 @@
 variable "project_id" {
   description = "The ID of the project in which the resource belongs"
   type        = string
-  default     = "test-project-ai"
+  default     = "test-project-ai-435808"
 }
 variable "workbench_region" {
   description = "The region of vertext ai workbench in which the resource belongs"
